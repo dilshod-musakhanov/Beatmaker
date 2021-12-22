a0d41aa9cd0de940c0f586a9ca74abed470e47ea
Dilshod Musakhanov <musakhanov@yahoo.com>
2021-12-22 09:29:25 +0400

    add inital sound

--- app.js
+++ app.js
@@ -11,12 +11,30 @@ class Drumkit {
     activePad() {
         this.classList.toggle("active");
     }
+    
+
     repeat(){
         let step = this.index % 8;
         const activeBars = document.querySelectorAll(`.b${step}`);
-        this.index++;
-        console.log(step);
-    }
+        activeBars.forEach(bar => {
+            bar.style.animation = `playTrack 0.3s alternate ease-in-out 2`;
+            if (bar.classList.contains("active")) {
+              if (bar.classList.contains("kick-pad")) {
+                this.kickAudio.currentTime = 0;
+                this.kickAudio.play();
+              }
+              if (bar.classList.contains("snare-pad")) {
+                this.snareAudio.currentTime = 0;
+                this.snareAudio.play();
+              }
+              if (bar.classList.contains("hihat-pad")) {
+                this.hihatAudio.currentTime = 0;
+                this.hihatAudio.play();
+              }
+            }
+          });
+          this.index++;
+        }
 
     start(){
         const interval = (60/this.bpm)*1000;
@@ -27,9 +45,13 @@ class Drumkit {
 }
 
 const drumkit = new Drumkit;
-drumkit.pads.forEach(pad =>{
+
+drumkit.pads.forEach(pad => {
     pad.addEventListener("click", drumkit.activePad);
-})
+    pad.addEventListener("animationend", function() {
+        this.style.animation = "";
+    });
+});
 
 drumkit.playButton.addEventListener("click", ()=>{
     drumkit.start();
